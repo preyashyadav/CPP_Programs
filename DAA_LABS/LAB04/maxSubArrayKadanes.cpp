e112a5348d296d888082f883b7196c001616e082
Preyash-tryin-to-code <preyashyadav29@gmail.com>
2022-02-28 15:42:19 +0530

    add matrix chain multiplication using dynamic programming

--- DAA_LABS/LAB04/maxSubArrayKadanes.cpp
+++ DAA_LABS/LAB04/maxSubArrayKadanes.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 int kadanes(int arr[],int n)
 {
- int maxsofar=INT_MIN;
- int maxending=0;
- for(int i=0;i<n;i++)
- {
- maxending+=arr[i];
- if(maxsofar<maxending)
- {
- maxsofar=maxending;
- }
- if(maxending<0)
- {
- maxending=0;
- }
- }
- return maxsofar;
+    int maxsofar=INT_MIN;
+    int maxending=0;
+    for(int i=0;i<n;i++)
+    {
+        maxending+=arr[i];
+        if(maxsofar<maxending)
+        {
+            maxsofar=maxending;
+        }
+        if(maxending<0)
+        {
+            maxending=0;
+        }
+    }
+    return maxsofar;
 }
 int main()
 {
